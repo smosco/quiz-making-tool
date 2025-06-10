bfa6363260230f42940ab21817725e3a11b662be
smosco <go1ruf2tk3@gmail.com>
2025-06-10 10:21:06 +0900

    feat: 정렬 툴바 표시 조건 계산 로직 추가

--- src/components/Toolbar/ToolbarManager.tsx
+++ src/components/Toolbar/ToolbarManager.tsx
@@ -1,3 +1,4 @@
+import { useSelectedObjects } from '../../store/useEditorStore';
 import useToolbarStore from '../../store/useToolbarStore';
 import { AlignmentToolbar } from './AlignmentToolbar';
 import DefaultToolbar from './DefaultToolbar';
@@ -5,15 +6,18 @@ import GroupToolbar from './GroupToolbar';
 import TextToolbar from './TextToolbar';
 
 export default function ToolbarManager() {
-  const { toolbarType } = useToolbarStore();
+  const toolbarType = useToolbarStore((state) => state.toolbarType);
+  const selectedObjects = useSelectedObjects();
+
+  const showAlignmentToolbar =
+    toolbarType === 'group' || selectedObjects.length >= 2;
 
   return (
     <div className='flex'>
       <DefaultToolbar />
       {toolbarType === 'text' && <TextToolbar />}
       {toolbarType === 'group' && <GroupToolbar />}
-      {/* TODO(@smosco): 정렬 툴바는 언제 등장해야하는가 */}
-      <AlignmentToolbar />
+      {showAlignmentToolbar && <AlignmentToolbar />}
     </div>
   );
 }
--- src/store/useToolbarStore.ts
+++ src/store/useToolbarStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { getEditorState } from './useEditorStore';
 
 export type ToolbarType = 'default' | 'text' | 'group' | 'image';
 
@@ -6,12 +7,25 @@ type ToolbarState = {
   toolbarType: ToolbarType;
   setToolbar: (type: ToolbarType) => void;
   resetToolbar: () => void;
+
+  // 정렬 툴바 표시 여부를 계산하는 함수
+  shouldShowAlignmentToolbar: () => boolean;
 };
 
-const useToolbarStore = create<ToolbarState>((set) => ({
+const useToolbarStore = create<ToolbarState>((set, get) => ({
   toolbarType: 'default',
+
   setToolbar: (type) => set({ toolbarType: type }),
+
   resetToolbar: () => set({ toolbarType: 'default' }),
+
+  // 정렬 툴바 표시 조건 계산
+  shouldShowAlignmentToolbar: () => {
+    const { toolbarType } = get();
+    const { selectedObjects } = getEditorState();
+
+    return toolbarType === 'group' || selectedObjects.length >= 2;
+  },
 }));
 
 export default useToolbarStore;
